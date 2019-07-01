c328032e6b54494cf436eb0859eeb01fd1afda84
Sergey Susev <susev.s.l@sberbank.ru>
2019-07-01 23:37:12 +0500

    Второе занятие, окончание

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -1,12 +1,13 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
 
 /**
  *
  * @author Sergey Susev
  */
-class User(
+data class User(
     val id: String,
     var firstName: String?,
     var lastName: String?,
@@ -34,11 +35,10 @@ class User(
 
     companion object Factory {
         private var lastId: Int = -1
-        fun makeUser(fullName: String?): User { //todo FixMe
+        fun makeUser(fullName: String?): User {
             lastId++
-            val parts: List<String>? = fullName?.split(" ")
-            val firstName = parts?.getOrNull(0)
-            val lastName = parts?.getOrNull(1)
+
+            val (firstName, lastName) = Utils.parseFullName(fullName)
             return User(id = "$lastId", firstName = firstName, lastName = lastName)
         }
     }
