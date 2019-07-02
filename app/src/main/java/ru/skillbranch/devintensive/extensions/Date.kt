b6810bc77df2bd92b0e3e85d62ea35528012ea49
Susev Sergey <sergejsusev@yandex.ru>
2019-07-02 13:03:53 +0500

    ДЗ со *

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -2,11 +2,14 @@ package ru.skillbranch.devintensive.extensions
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.abs
 
 const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
 const val HOUR = 60 * MINUTE
 const val DAY = 24 * HOUR
+const val BEFORE = " назад"
+const val AFTER = "через "
 
 
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
@@ -34,7 +37,51 @@ enum class TimeUnits {
     DAY
 }
 
-
+/**
+ * Метод отображения последнего визита в человекоподобном виде
+ * 0с - 1с "только что"
+ * 1с - 45с "несколько секунд назад"
+ * 45с - 75с "минуту назад"
+ * 75с - 45мин "N минут назад"
+ * 45мин - 75мин "час назад"
+ * 75мин 22ч "N часов назад"
+ * 22ч - 26ч "день назад"
+ * 26ч - 360д "N дней назад"
+ * >360д "более года назад"
+ */
 fun Date.humanizeDiff(date: Date = Date()): String {
-    return "HumaniceDiff"
+    var diffTime = date.time - this.time
+    val isAfter = diffTime < 0
+    diffTime = abs(diffTime)
+    val after = if (isAfter) AFTER else ""
+    val before = if (!isAfter) BEFORE else ""
+    return when (diffTime) {
+        in 0..SECOND -> "только что"
+        in SECOND..45 * SECOND -> "несколько секунд"
+
+        in 45 * SECOND..75 * SECOND -> after + "минуту"
+        21 * MINUTE, 31 * MINUTE, 41 * MINUTE -> after + "${diffTime / MINUTE} минуту"
+
+        in 2 * MINUTE..4 * MINUTE, in 21 * MINUTE..25 * MINUTE, in 31 * MINUTE..35 * MINUTE, in 41 * MINUTE..45 * MINUTE
+        -> after + "${diffTime / MINUTE} минуты"
+        in 4 * MINUTE..21 * MINUTE, in 25 * MINUTE..31 * MINUTE, in 35 * MINUTE..41 * MINUTE
+        -> after + "${diffTime / MINUTE} минут"
+
+        in 45 * MINUTE..75 * MINUTE -> after + "час"
+        in 75 * MINUTE..4 * HOUR -> after + "${diffTime / HOUR} часа"
+        in 4 * HOUR..20 * HOUR -> after + "${diffTime / HOUR} часов"
+        in 20 * HOUR..22 * HOUR -> after + "${diffTime / HOUR} час"
+
+        in 22 * HOUR..26 * HOUR -> after + "день"
+        in 10 * DAY..15 * DAY -> after + "${diffTime / DAY} дней"
+        in 26 * HOUR..360 * DAY -> {
+            val dayLit = when ((diffTime / DAY) % 10) {
+                1L -> "день"
+                in 1L..4L -> "дня"
+                else -> "дней"
+            }
+            after + "${diffTime / DAY} $dayLit"
+        }
+        else -> if (isAfter) "более чем через год" else "более года"
+    } + before
 }
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,6 +1,42 @@
 package ru.skillbranch.devintensive.utils
 
 object Utils {
+    private var mapLetter = mutableMapOf(
+        'а' to "a",
+        'б' to "b",
+        'в' to "v",
+        'г' to "g",
+        'д' to "d",
+        'е' to "e",
+        'ё' to "e",
+        'ж' to "zh",
+        'з' to "z",
+        'и' to "i",
+        'й' to "i",
+        'к' to "k",
+        'л' to "l",
+        'м' to "m",
+        'н' to "n",
+        'о' to "o",
+        'п' to "p",
+        'р' to "r",
+        'с' to "s",
+        'т' to "t",
+        'у' to "u",
+        'ф' to "f",
+        'х' to "h",
+        'ц' to "c",
+        'ч' to "ch",
+        'ш' to "sh",
+        'щ' to "sh'",
+        'ъ' to "",
+        'ы' to "i",
+        'ь' to "",
+        'э' to "e",
+        'ю' to "yu",
+        'я' to "ya"
+    )
+
     fun parseFullName(fullName: String?): Pair<String?, String?> {
 //        TODO FIX ME
         val parts: List<String>? = fullName?.split(" ")
@@ -10,10 +46,28 @@ object Utils {
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
-        return "SUSEV SERGEY"
+        mapLetter[' '] = divider
+        val parseFullName = parseFullName(payload)
+        var resultFirstName = ""
+        var resultLastName = ""
+        parseFullName.first?.toLowerCase()?.toCharArray()?.forEach { c ->
+            if (mapLetter.containsKey(c)) resultFirstName += mapLetter[c] else resultFirstName += c
+        }
+        parseFullName.second?.toLowerCase()?.toCharArray()?.forEach { c ->
+            if (mapLetter.containsKey(c)) resultLastName += mapLetter[c] else resultLastName += c
+        }
+        resultFirstName = resultFirstName[0].toUpperCase() + resultFirstName.substring(1)
+        resultLastName = resultLastName[0].toUpperCase() + resultLastName.substring(1)
+        return "$resultFirstName$divider$resultLastName"
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        return "СС"
+        if (firstName == null && lastName == null) return null
+        val firstLetter = firstName?.trim()?.toUpperCase()?.firstOrNull()
+        val secondLetter = lastName?.trim()?.toUpperCase()?.firstOrNull()
+        if (firstLetter == null && secondLetter == null) return null
+        if (firstLetter == null) return secondLetter.toString()
+        if (secondLetter == null) return firstLetter.toString()
+        return "$firstLetter$secondLetter"
     }
 }
